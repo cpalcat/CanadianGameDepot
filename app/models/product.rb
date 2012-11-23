14e3890ff9fe48dd58da3d65f6e98d3a0ed5c84d
Colton <cpalcat@academic.rrc.ca>
2012-11-23 16:11:06 -0600

    fixed new product issues

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
-    belongs_to :category
+    belongs_to :category, :foreign_key => 'category'
 		
     has_many :orders, :through => :line_items
+    
+    validates :title, 
 end
